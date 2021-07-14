1d57f0303623007d521ade016296f159cf945520
Kyrylo Lvov ⚡ <70720713+kyrylolvov@users.noreply.github.com>
2021-07-14 09:50:10 +0300

    Update Me.js

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,8 +1,37 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React from "react";
 
-const Me = (props) => {
-  const [output, setOutput] = useState("");
-  return <div></div>;
-};
+class Me extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageMessage: "Loading data, please wait...",
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      axios
+        .get("http://142.93.134.108:1111/me", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+        })
+        .then((res) => {
+          this.setState({
+            pageMessage: res.data.body.message,
+          });
+        });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <strong className="page__message">🔥 {this.state.pageMessage}</strong>
+      </div>
+    );
+  }
+}
 
 export default Me;
